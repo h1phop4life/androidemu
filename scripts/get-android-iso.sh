6dee88db1d638b9fbe5952c9eda7a335379beb14
h1phop4life <helpinstagram@mail.com>
2025-08-17 03:31:12 +0300

    Update get-android-iso.sh

--- scripts/get-android-iso.sh
+++ scripts/get-android-iso.sh
@@ -2,7 +2,9 @@
 set -e
 cd "$(dirname "$0")/.."
 
-ISO_URL="${1:-https://mirrors.ocf.berkeley.edu/android-x86/4.4-r5/android-x86-4.4-r5.iso}"
+# önerilen mirror (SourceForge)
+ISO_URL="${1:-https://sourceforge.net/projects/android-x86/files/Release%204.4/android-x86-4.4-r5.iso/download}"
+
 OUT="data/android-x86.iso"
 
 echo "[*] ISO indiriliyor: $ISO_URL"
